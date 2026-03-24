7358d6f670753483ac39c40f68f5d10c951ea70c
Tanbiru <tanvir12tt@gmail.com>
2026-03-24 06:31:14 +0600

    Complete navbar component

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import logo from "/src/assets/logo.png"
+import { FaGithub } from 'react-icons/fa6';
 
 const Navbar = () => {
+    const links = <>
+    <li className='m-2 font-semibold text-sm'>Home</li>
+    <li className='m-2 font-semibold text-sm'>Apps</li>
+    <li className='m-2 font-semibold text-sm'>Installation</li>
+    
+    </>
     return (
-        <div>
-            <h1>Hello ami navbar</h1>
+        <div className='w-10/12 mx-auto'>
+            <div className="navbar">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        {links}
+      </ul>
+    </div>
+    
+       <a className="btn btn-ghost flex items-center gap-2 normal-case px-2 md:px-4">
+    <img 
+        className="w-8 h-8 md:w-10 md:h-10 object-contain" 
+        src={logo} 
+        alt="logo" 
+    />
+    <span className="text-sm md:text-lg font-bold text-[#632EE3]">
+        HERO.IO
+    </span>
+</a>
+    
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {links}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-[#FFFFFF]"><FaGithub></FaGithub> Contribute</a>
+  </div>
+</div>
         </div>
     );
 };
