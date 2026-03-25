bd67741e30c84c51f322e672d002db609ecb48aa
Tanbiru <tanvir12tt@gmail.com>
2026-03-25 21:27:23 +0600

    Footer Added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
 const Footer = () => {
+  
     return (
-        <div>
-            <h1>ami footer</h1>
-        </div>
+       <div className='bg-black'>
+        <footer className="footer sm:footer-horizontal  text-neutral-content p-10 mt-20 w-10/12 mx-auto">
+  <nav>
+    
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+   
+    <a className="link link-hover">Home</a>
+    <a className="link link-hover">Apps</a>
+    <a className="link link-hover">Installation</a>
+    
+  </nav>
+  <nav>
+   
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+       </div>
     );
 };
 
